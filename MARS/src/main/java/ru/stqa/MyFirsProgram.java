a62efc2b223291611896dd65aea9fc4f99015495
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-03-17 07:47:26 +0300

    1

--- MARS/src/main/java/ru/stqa/MyFirsProgram.java
+++ MARS/src/main/java/ru/stqa/MyFirsProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa;
 
 public class MyFirsProgram {
-    public static void main (String[]args){
+    public static void main(String[] args) {
         System.out.println("Hello,word!");
     }
 }
