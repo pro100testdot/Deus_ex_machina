0c288632184362ede30209c1ab226a09e934b64e
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-03-17 08:02:09 +0300

    добавление матиматики

---
+++ MARS/src/main/java/ru/stqa/Mat.java
@@ -0,0 +1,11 @@
+package ru.stqa;
+
+public class Mat {
+    public static void main(String[] args) {
+        System.out.println(5 + 5);
+        System.out.println(2 * 2);
+        System.out.println(3 - 2);
+        System.out.println(4 / 2);
+        System.out.println(1.0 / 2);
+    }
+}
--- MARS/src/main/java/ru/stqa/MyFirsProgram.java
+++ MARS/src/main/java/ru/stqa/MyFirsProgram.java
@@ -3,8 +3,6 @@ package ru.stqa;
 public class MyFirsProgram {
     public static void main(String[] args) {
         System.out.println("Hello,word!");
-
-        System.out.println(5 + 5);
-        System.out.println(2 - 2);
     }
+
 }
