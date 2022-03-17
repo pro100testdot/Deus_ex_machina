2de482afb861399e32acf4ff0e45ad5fbb9cdb44
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-03-17 07:54:32 +0300

    дополнительные функции

--- MARS/src/main/java/ru/stqa/MyFirsProgram.java
+++ MARS/src/main/java/ru/stqa/MyFirsProgram.java
@@ -3,5 +3,8 @@ package ru.stqa;
 public class MyFirsProgram {
     public static void main(String[] args) {
         System.out.println("Hello,word!");
+
+        System.out.println(5 + 5);
+        System.out.println(2 - 2);
     }
 }
