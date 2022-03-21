95eb65168a942ae6e1a73c73152dfa7af1297948
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-03-21 20:21:13 +0300

    изучение переменных

--- MARS/src/main/java/ru/stqa/MyFirsProgram.java
+++ MARS/src/main/java/ru/stqa/MyFirsProgram.java
@@ -2,7 +2,7 @@ package ru.stqa;
 
 public class MyFirsProgram {
     public static void main(String[] args) {
-        System.out.println("Hello,word!");
+        System.out.println("Hello, word!");
     }
 
 }
