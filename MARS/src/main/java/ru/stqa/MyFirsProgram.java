d6f477eddb97bc7c8ddb36a6d4596329bba5b9c9
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-03-31 17:37:08 +0300

    добавлены функции вычесления

--- MARS/src/main/java/ru/stqa/MyFirsProgram.java
+++ MARS/src/main/java/ru/stqa/MyFirsProgram.java
@@ -2,7 +2,30 @@ package ru.stqa;
 
 public class MyFirsProgram {
     public static void main(String[] args) {
-        System.out.println("Hello, word!");
+        hello("word");
+        hello("max");
+        hello("men");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стророной " + l + " = " + area(l));
+        double len = 5;
+
+        double a = 5;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b +" = "+ area(a,b));
+
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 
 }
