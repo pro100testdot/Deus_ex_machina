17eb1b7f25f31d9a6242d5edd21ff4884a0fa8d0
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-04-07 21:35:14 +0300

    1111

---
+++ MARS/src/main/java/ru/stqa/Helper.java
@@ -0,0 +1,25 @@
+
+public class Helper {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(10,10);
+        System.out.println(p1.distance(p2));
+        System.out.println(p2.distance(p1));
+    }
+
+}
+
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x1, double y1) {
+        this.x = x1;
+        this.y = y1;
+    }
+
+    public double distance(Point p2) {
+        return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
+    }
+}
