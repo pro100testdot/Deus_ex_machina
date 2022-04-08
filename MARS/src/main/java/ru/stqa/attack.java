cc9cb3d8557fceaae7a7a863a4e9f01bf6b8b163
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-04-08 19:44:19 +0300

    мои изменения

---
+++ MARS/src/main/java/ru/stqa/attack.java
@@ -0,0 +1,9 @@
+package ru.stqa;
+
+public class attack {
+
+    public static void main(String[] args) {
+        attackBase a = new attackBase(2, 3, 4, 6);
+        System.out.println("расчет атаки магичнской атаки производиться так" + a.f + a.w+  " = " );
+    }
+}
