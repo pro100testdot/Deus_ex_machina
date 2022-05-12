91d5db040dd044570b11627c1de03fdc4ed71448
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-05-12 18:40:33 +0300

    Исправление теста

--- MARS/src/main/java/ru/stqa/BasePoint.java
+++ MARS/src/main/java/ru/stqa/BasePoint.java
@@ -1,17 +1,17 @@
 package ru.stqa;
 
 public class BasePoint {
-    public double x1;
-    public double y1;
-    public double x2;
-    public double y2;
+    public double x1, y1, x2, y2;
+
     public BasePoint(double x1, double y1, double x2, double y2) {
-        this.x1=x1;
-        this.y1=y1;
-        this.x2=x2;
-        this.y2=y2;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
 
     }
-
+    public double distance() {
+        return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
 }
 
--- MARS/src/main/java/ru/stqa/Points.java
+++ MARS/src/main/java/ru/stqa/Points.java
@@ -3,11 +3,9 @@ package ru.stqa;
 public class Points {
     public static void main(String[] args) {
 
-        BasePoint B = new BasePoint(3, 3, 6, 6);
-        System.out.println("points distance =" + distance(B));
+        BasePoint b = new BasePoint(3, 3, 6, 6);
+        System.out.println("points distance = " + b.distance());
     }
-    public static double distance (BasePoint P){
-        return Math.sqrt((P.x2 - P.x1) * (P.x2 - P.x1) + (P.y2 - P.y1) * (P.y2 - P.y1));
     }
-}
+
 
