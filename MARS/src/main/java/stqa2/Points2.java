c5572b80d3e0b9e5a08970ab55d8b3f0503dc100
pro100testdot <67010615+pro100testdot@users.noreply.github.com>
2022-06-06 22:33:22 +0300

    1

---
+++ MARS/src/main/java/stqa2/Points2.java
@@ -0,0 +1,13 @@
+package stqa2;
+
+public class Points2 {
+    public static void main(String[] args) {
+
+        BasePoint2 p1 = new BasePoint2(3, 3);
+        BasePoint2 p2 = new BasePoint2(5, 5);
+        System.out.println(p1.distance(p2));
+
+    }
+}
+
+
